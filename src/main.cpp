9dd6b14c11b023abeee5403db135f154fe92a3aa
Jaedan Calder <jcalder@hammond.com.au>
2023-06-07 11:34:26 +1000

    CMake uses new Python3 find module. dll copied to build for debugging.

--- src/main.cpp
+++ src/main.cpp
@@ -212,7 +212,7 @@ int main(int, char**)
     //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
     //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
     //IM_ASSERT(font != nullptr);
-    demo_fonts_init();
+    // demo_fonts_init();
 
     // Our state
     bool show_demo_window = true;
